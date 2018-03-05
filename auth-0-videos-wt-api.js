1f871d1129fd3407e41479e0f7768318ace90d47
Luke Oliff <luke@lukeoliff.com>
2018-03-05 18:18:44 +0000

    wip

--- auth-0-videos-wt-api.js
+++ auth-0-videos-wt-api.js
@@ -5,19 +5,44 @@ const jwksRsa = require('jwks-rsa');
 const mongoose = require('mongoose');
 const wt = require('webtask-tools');
 
+console.log(wt);
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  const issuer = 'https://' + req.webtaskContext.secrets.AUTH0_DOMAIN + '/';
+  [AUTH0_DOMAIN, AUTH0_API_AUDIENCE] = req.webtaskContext.secrets;
+  const issuer = `https://${AUTH0_DOMAIN}/`;
   jwt({
-    secret: jwksRsa.expressJwtSecret({ jwksUri: issuer + '.well-known/jwks.json' }),
-    audience: req.webtaskContext.secrets.AUDIENCE,
+    secret: jwksRsa.expressJwtSecret({ jwksUri: `${issuer}.well-known/jwks.json` }),
+    audience: AUTH0_API_AUDIENCE,
     issuer: issuer,
     algorithms: [ 'RS256' ]
   })(req, res, next);
+});
+
+app.use((req, res, next) => {
+  const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+  [DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT] = req.webtaskContext.secrets;
+  const mongodbUri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+  mongoose.connect(mongodbUri, options);
+
+  var VideoSchema = new Schema({
+    name: String,
+    id: String
+  });
+
+  var WatchSchema = new Schema({
+    user: String,
+    video: VideoSchema
+  });
+
+  mongoose.model('Video', VideoSchema);
+  mongoose.model('Watch', WatchSchema);
 });
 
 app.get('/test', (req, res) => {
